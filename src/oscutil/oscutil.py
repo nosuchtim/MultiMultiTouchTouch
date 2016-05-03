e8f459ee1b7d3c0676bf10d5eec6b416f70f185e
Tim Thompson <me@timthompson.com>
2016-05-02 23:31:53 -0700

    add quadrants_kinect2, handle TUIO source messages in osclisten

--- src/oscutil/oscutil.py
+++ src/oscutil/oscutil.py
@@ -20,9 +20,10 @@ SLIP_END = chr(192)
 SLIP_ESC = chr(219)
 SLIP_ESC2 = chr(221)
 
-global Showalive,Showfseq
+global Showalive,Showfseq,Showsource
 Showalive = False
 Showfseq = False
+Showsource = False
 
 def nextSLIPMsg(data):
 	first = data[0]
@@ -323,6 +324,8 @@ def handleonemessage(m):
 		return
 	if Showfseq==False and len(m) >= 3 and m[2] == "fseq":
 		return
+	if Showsource==False and len(m) >= 3 and m[2] == "source":
+		return
 	# global time0
 	# print ("%8.3f " % (time.time()-time0)) + m.__str__()
 	print m
@@ -389,6 +392,7 @@ if __name__ == '__main__':
 			usage()
 		Showalive = False
 		Showfseq = False
+		Showsource = False
 		dolisten(sys.argv[2])
 
 	elif command == "listenall":
@@ -396,6 +400,7 @@ if __name__ == '__main__':
 			usage()
 		Showalive = True
 		Showfseq = True
+		Showsource = True
 		dolisten(sys.argv[2])
 
 	elif command == "record":
